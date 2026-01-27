061734cd8a632408cbeaff78207e2578b5984988
Vishnu Tilak <29648840+vishnutilak@users.noreply.github.com>
2026-01-27 11:55:35 +0530

    Create FizzBuzz Mini.py

---
+++ FizzBuzz Mini.py
@@ -0,0 +1,10 @@
+def fizz_buzz_mini(n):
+
+    if n%15==0:
+        return "FizzBuzz"
+    elif n%5==0:
+        return "Buzz"
+    elif n%3==0:
+        return "Fizz"
+    else:
+        return str(n)
