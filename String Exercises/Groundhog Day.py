155a66d49751ec9220ce26938ce081b18b513c74
Vishnu Tilak <29648840+vishnutilak@users.noreply.github.com>
2026-02-03 10:54:38 +0530

    Create Groundhog Day.py

---
+++ String Exercises/Groundhog Day.py
@@ -0,0 +1,7 @@
+def groundhog_day_prediction(appearance):
+    if appearance== True:
+        return "Looks like we'll have six more weeks of winter."
+    if appearance== False:
+        return "It's going to be an early spring."
+    else:
+        return "No prediction this year."
